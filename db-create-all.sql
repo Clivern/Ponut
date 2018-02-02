514133e98ca532ad33464ae6cd1b9111669c6a74
Clivern <hello@clivern.com>
2018-02-02 11:05:36 +0100

    Remove db from app root

--- db-create-all.sql
+++
@@ -1,8 +0,0 @@
-create table options (
-  id                            bigint auto_increment not null,
-  opkey                         varchar(255) not null,
-  opvalue                       varchar(255) not null,
-  autoload                      varchar(255) not null,
-  constraint pk_options primary key (id)
-);
-
